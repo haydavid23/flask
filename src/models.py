937a90d39212422d59a5bd67d1d7c82d4055d55d
haydavid23 <hay.david@hotmail.com>
2019-05-16 23:48:15 +0000

    first

--- src/models.py
+++ src/models.py
@@ -4,7 +4,7 @@ db = SQLAlchemy()
 
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    username = db.Column(db.String(81), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
 
     def __repr__(self):
